e4679fb850b0a81f62c439c5ed014cfc562e63d3
Joanjajas <joanbeltranperis@gmail.com>
2023-10-05 12:03:08 +0200

    added script to scrap exams info from intranet

---
+++ scrapper.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def main():
+    # Input login credentials
+    username = input("Username: ")
+    password = input("Password: ")
+
+    # Create a new instance of the Safari web driver
+    driver = webdriver.Safari()
+
+    # Define implicit wait
+    driver_wait = WebDriverWait(driver, 10)
+
+    # Login and navigate to grades page
+    login(driver, username, password)
+    navigate_to_grades_page(driver_wait)
+
+
+def login(driver, username, password):
+    # Go to the login page
+    driver.get("https://intranet.upv.es")
+
+    # Login
+    form = driver.find_element(By.XPATH, "//form[@name='alumno']")
+    form.find_element(By.XPATH, "//input[@name='dni']").send_keys(username)
+    form.find_element(By.XPATH, "//input[@name='clau']").send_keys(password)
+    form.find_element(By.XPATH, "//input[@class='upv_btsubmit']").click()
+
+
+def navigate_to_grades_page(driver_wait):
+    # Enter intranet
+    driver_wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@id='intranet']//a[2]"))).click()
+
+    # Enter grades page
+    driver_wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@id='subgrupo_402']//table[@id='elemento_405']//a"))).click()
+
+
+if __name__ == "__main__":
+    main()
